704f614ef28f021905a8875ac649799ddd6c8512
RAVISAN1 <165484581+RAVISAN1@users.noreply.github.com>
2025-03-08 09:35:14 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -24,45 +24,7 @@ def getenv(var):
 
 
 ##########################################################
-# modiji.site
 
-
-def modiji.site(url):
-    client = cloudscraper.create_scraper(allow_brotli=False)
-    if "https://modiji.site" in url:
-        DOMAIN = "https://apk1dm.com/"
-    else:
-        DOMAIN = "https://modiji.site"
-
-    url = url[:-1] if url[-1] == "/" else url
-
-    code = url.split("/")[-1]
-    if "rocklinks.net" in url:
-        final_url = f"{DOMAIN}/{code}?quelle="
-    else:
-        final_url = f"{DOMAIN}/{code}"
-
-    resp = client.get(final_url)
-    soup = BeautifulSoup(resp.content, "html.parser")
-
-    try:
-        inputs = soup.find(id="go-link").find_all(name="input")
-    except:
-        return "Incorrect Link"
-
-    data = {input.get("name"): input.get("value") for input in inputs}
-
-    h = {"x-requested-with": "XMLHttpRequest"}
-
-    time.sleep(10)
-    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
-    try:
-        return r.json()["url"]
-    except:
-        return "Something went wrong :("
-
-
-################################################
 # ENVs
 
 GDTot_Crypt = getenv("CRYPT")
